29601766e7d4d301d5fe95d8bd201051264d2812
michalK00 <mklemens48@gmail.com>
2024-11-20 01:24:34 +0100

    add logger and redis client factory methods

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	github.com/spf13/cobra v1.8.1
 	github.com/swaggo/swag v1.16.3
+	go.uber.org/zap v1.27.0
 )
 
 require (
@@ -27,9 +28,13 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.28.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.32.2 // indirect
 	github.com/aws/smithy-go v1.22.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/redis/go-redis/v9 v9.7.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	gopkg.in/go-jose/go-jose.v2 v2.6.3 // indirect
 )
 
--- internal/cmdutil/cmdutil.go
+++ internal/cmdutil/cmdutil.go
@@ -2,11 +2,23 @@ package cmdutil
 
 import (
 	"context"
+	"github.com/redis/go-redis/v9"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.uber.org/zap"
 	"os"
 	"time"
 )
+
+func NewLogger(service string) *zap.Logger {
+	env := os.Getenv("ENV")
+	logger, _ := zap.NewProduction(zap.Fields(zap.String("env", env), zap.String("service", service)))
+
+	if env == "" || env == "development" {
+		logger, _ = zap.NewDevelopment()
+	}
+	return logger
+}
 
 func NewMongoClient() (*mongo.Database, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -21,4 +33,18 @@ func NewMongoClient() (*mongo.Database, error) {
 	}
 
 	return client.Database(os.Getenv("MONGODB_NAME")), nil
+}
+
+func NewRedisClient(ctx context.Context) (*redis.Client, error) {
+	opt, err := redis.ParseURL(os.Getenv("REDIS_URL"))
+	if err != nil {
+		return nil, err
+	}
+	client := redis.NewClient(opt)
+
+	if err := client.Ping(ctx).Err(); err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
