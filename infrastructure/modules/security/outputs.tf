5e5b4423723ce1c184758c8fd648d596d4d5c22f
michalK00 <mklemens48@gmail.com>
2025-06-09 16:02:20 +0200

    workflow fix

--- infrastructure/modules/security/outputs.tf
+++ infrastructure/modules/security/outputs.tf
@@ -1,15 +1,3 @@
-output "developer_access_key_id" {
-  description = "Access key ID for developer user"
-  value       = aws_iam_access_key.developer.id
-  sensitive   = true
-}
-
-output "developer_secret_access_key" {
-  description = "Secret access key for developer user"
-  value       = aws_iam_access_key.developer.secret
-  sensitive   = true
-}
-
 output "ecs_task_execution_role_arn" {
   description = "ARN of the ECS task execution role"
   value       = aws_iam_role.ecs_task_execution.arn
